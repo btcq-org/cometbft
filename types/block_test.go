3d197d9cb02ab766bf6b23158851095f8e121d19
Johnny Luo <johnnyluo1980@gmail.com>
2025-10-11 21:40:43 +1100

    feat: enhance MLDSA support with new tests and parameter adjustments

--- types/block_test.go
+++ types/block_test.go
@@ -466,10 +466,9 @@ func TestBlockMaxDataBytes(t *testing.T) {
 		0: {-10, 1, 0, true, 0},
 		1: {10, 1, 0, true, 0},
 		2: {841, 1, 0, true, 0},
-		3: {842, 1, 0, false, 0},
-		4: {843, 1, 0, false, 1},
-		5: {954, 2, 0, false, 1},
-		6: {1053, 2, 100, false, 0},
+		3: {3200, 1, 0, false, 0},
+		4: {3201, 1, 0, false, 1},
+		5: {5669, 2, 0, false, 0},
 	}
 
 	for i, tc := range testCases {
@@ -497,8 +496,8 @@ func TestBlockMaxDataBytesNoEvidence(t *testing.T) {
 		0: {-10, 1, true, 0},
 		1: {10, 1, true, 0},
 		2: {841, 1, true, 0},
-		3: {842, 1, false, 0},
-		4: {843, 1, false, 1},
+		3: {3200, 1, false, 0},
+		4: {3201, 1, false, 1},
 	}
 
 	for i, tc := range testCases {
