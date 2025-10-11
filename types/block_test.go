a897dbc96f7cb28391421ab1185615f37d124ec4
Johnny Luo <johnnyluo1980@gmail.com>
2025-10-11 21:40:43 +1100

    feat: enhance MLDSA support with new tests and parameter adjustments

--- types/block_test.go
+++ types/block_test.go
@@ -467,9 +467,9 @@ func TestBlockMaxDataBytes(t *testing.T) {
 		0: {-10, 1, 0, true, 0},
 		1: {10, 1, 0, true, 0},
 		2: {841, 1, 0, true, 0},
-		3: {875, 1, 0, false, 0},
-		4: {876, 1, 0, false, 1},
-		5: {1019, 2, 0, false, 0},
+		3: {3200, 1, 0, false, 0},
+		4: {3201, 1, 0, false, 1},
+		5: {5669, 2, 0, false, 0},
 	}
 
 	for i, tc := range testCases {
@@ -497,8 +497,8 @@ func TestBlockMaxDataBytesNoEvidence(t *testing.T) {
 		0: {-10, 1, true, 0},
 		1: {10, 1, true, 0},
 		2: {841, 1, true, 0},
-		3: {875, 1, false, 0},
-		4: {876, 1, false, 1},
+		3: {3200, 1, false, 0},
+		4: {3201, 1, false, 1},
 	}
 
 	for i, tc := range testCases {
--- types/params.go
+++ types/params.go
@@ -125,7 +125,7 @@ func DefaultEvidenceParams() EvidenceParams {
 // only ed25519 pubkeys.
 func DefaultValidatorParams() ValidatorParams {
 	return ValidatorParams{
-		PubKeyTypes: []string{ABCIPubKeyTypeMLDSA44},
+		PubKeyTypes: []string{ABCIPubKeyTypeMLDSA44, ABCIPubKeyTypeEd25519},
 	}
 }
 
