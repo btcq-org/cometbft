7e2e7537c9ef5f4e76ef0a09d74e06a518d92004
Johnny Luo <johnnyluo1980@gmail.com>
2026-01-19 15:13:35 +1100

    refactor: remove unused BLS12381 key type from params

--- types/params.go
+++ types/params.go
@@ -24,19 +24,12 @@ const (
 
 	ABCIPubKeyTypeEd25519   = ed25519.KeyType
 	ABCIPubKeyTypeSecp256k1 = secp256k1.KeyType
-	ABCIPubKeyTypeBls12381  = bls12381.KeyType
 	ABCIPubKeyTypeMLDSA44   = mldsa.KeyType
 )
 
 var ABCIPubKeyTypesToNames = map[string]string{
 	ABCIPubKeyTypeEd25519:   ed25519.PubKeyName,
 	ABCIPubKeyTypeSecp256k1: secp256k1.PubKeyName,
-}
-
-func init() {
-	if bls12381.Enabled {
-		ABCIPubKeyTypesToNames[ABCIPubKeyTypeBls12381] = bls12381.PubKeyName
-	}
 }
 
 // ConsensusParams contains consensus critical parameters that determine the
